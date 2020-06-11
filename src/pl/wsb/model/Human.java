59e26dc43a9cb568f94c2853a78d338932d05764
Patryk Rutkowski <patrynio0147@gmail.com>
2020-06-11 14:21:52 +0200

    Zadanie 3 i 4

--- src/pl/wsb/model/Human.java
+++ src/pl/wsb/model/Human.java
@@ -4,4 +4,20 @@ public class Human {
 
     private Animal pet;
     private Car car;
+    private Double salary;
+
+    public Double getSalary() {
+        System.out.println("Wypłata: " + this.salary);
+        return salary;
+    }
+
+    public void setSalary(double newSalary) {
+        if(newSalary < 0) {
+            System.err.println("Nie można podać ujemnej wartośći");
+        } else {
+            System.out.println("Nowe dane zostały wysłane");
+            System.out.println("Proszę odebrać aneks do umowy");
+            salary = newSalary;
+        }
+    }
 }
